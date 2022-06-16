2578c93d745c9ef577993be8d0a66569d8a24dad
cofre <cofre.le@gmail.com>
2022-06-16 09:50:26 -0300

    Persona y Producto

---
+++ Modelo/Class/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Class
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public DateTime Birth { get; set; }
+
+    }
+}
