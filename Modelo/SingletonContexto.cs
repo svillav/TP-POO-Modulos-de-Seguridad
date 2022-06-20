5f3585bd411ad0c0a2a035289502bebfd64eb90f
Martin Ballester <martinballester1@gmail.com>
2022-06-20 18:13:53 -0300

    dependencies MVC add to project, work on register form

---
+++ Modelo/SingletonContexto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class SingletonContext
+    {
+        private static SingletonContext _instance;
+        private static ContextoContainer _context;
+
+        private SingletonContext() { }
+
+        public static SingletonContext getInstance()
+        {
+            if( _instance == null )
+            {
+                _instance = new SingletonContext();
+                _context = new ContextoContainer();
+            }
+            return _instance;
+        }
+
+        public ContextoContainer Contexto
+        {
+            get { return _context; }
+        }
+    }
+}
