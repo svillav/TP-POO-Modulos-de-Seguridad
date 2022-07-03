424ffd74af2b5212534d38ba1763149d74a29637
Martin Ballester <martinballester1@gmail.com>
2022-07-03 13:30:15 -0300

    Task3: Delete fields on register form and fix tab index

--- Vista/Register.Designer.cs
+++ Vista/Register.Designer.cs
@@ -31,28 +31,21 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.txtRePassword = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
-            this.txtREmail = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtDNI = new System.Windows.Forms.TextBox();
-            this.txtLastName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.btnSubmit = new System.Windows.Forms.Button();
-            this.txtUser = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
-            this.label9 = new System.Windows.Forms.Label();
             this.UserType = new System.Windows.Forms.DomainUpDown();
+            this.UserTypeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(181, 109);
+            this.txtName.Location = new System.Drawing.Point(180, 109);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(206, 20);
             this.txtName.TabIndex = 0;
@@ -60,76 +53,51 @@
             // 
             // txtRePassword
             // 
-            this.txtRePassword.Location = new System.Drawing.Point(554, 267);
+            this.txtRePassword.Location = new System.Drawing.Point(180, 265);
             this.txtRePassword.Name = "txtRePassword";
             this.txtRePassword.PasswordChar = '•';
-            this.txtRePassword.Size = new System.Drawing.Size(198, 20);
-            this.txtRePassword.TabIndex = 1;
+            this.txtRePassword.Size = new System.Drawing.Size(206, 20);
+            this.txtRePassword.TabIndex = 4;
             this.txtRePassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(554, 227);
+            this.txtPassword.Location = new System.Drawing.Point(180, 225);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '•';
-            this.txtPassword.Size = new System.Drawing.Size(198, 20);
-            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Size = new System.Drawing.Size(206, 20);
+            this.txtPassword.TabIndex = 3;
             this.txtPassword.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            // 
-            // txtREmail
-            // 
-            this.txtREmail.Location = new System.Drawing.Point(554, 188);
-            this.txtREmail.Name = "txtREmail";
-            this.txtREmail.Size = new System.Drawing.Size(198, 20);
-            this.txtREmail.TabIndex = 3;
-            this.txtREmail.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(554, 148);
+            this.txtEmail.Location = new System.Drawing.Point(180, 145);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(198, 20);
-            this.txtEmail.TabIndex = 4;
+            this.txtEmail.Size = new System.Drawing.Size(206, 20);
+            this.txtEmail.TabIndex = 1;
             this.txtEmail.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // txtDNI
             // 
-            this.txtDNI.Location = new System.Drawing.Point(181, 190);
+            this.txtDNI.Location = new System.Drawing.Point(180, 186);
             this.txtDNI.Name = "txtDNI";
             this.txtDNI.Size = new System.Drawing.Size(206, 20);
-            this.txtDNI.TabIndex = 5;
+            this.txtDNI.TabIndex = 2;
             this.txtDNI.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
-            // 
-            // txtLastName
-            // 
-            this.txtLastName.Location = new System.Drawing.Point(181, 150);
-            this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(206, 20);
-            this.txtLastName.TabIndex = 6;
-            this.txtLastName.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(74, 112);
+            this.label1.Location = new System.Drawing.Point(63, 116);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 7;
             this.label1.Text = "Name";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(74, 153);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Last Name";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(74, 193);
+            this.label3.Location = new System.Drawing.Point(63, 193);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(26, 13);
             this.label3.TabIndex = 9;
@@ -138,25 +106,16 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(424, 232);
+            this.label4.Location = new System.Drawing.Point(63, 228);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Password";
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(424, 192);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(73, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Repeat E-mail";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(424, 151);
+            this.label6.Location = new System.Drawing.Point(63, 152);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(36, 13);
             this.label6.TabIndex = 10;
@@ -165,7 +124,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(424, 270);
+            this.label7.Location = new System.Drawing.Point(63, 266);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(91, 13);
             this.label7.TabIndex = 13;
@@ -173,7 +132,7 @@
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(316, 368);
+            this.btnSubmit.Location = new System.Drawing.Point(229, 356);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(106, 23);
             this.btnSubmit.TabIndex = 14;
@@ -181,72 +140,42 @@
             this.btnSubmit.UseVisualStyleBackColor = true;
             this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
-            // txtUser
-            // 
-            this.txtUser.Location = new System.Drawing.Point(554, 109);
-            this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(198, 20);
-            this.txtUser.TabIndex = 15;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(424, 112);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(29, 13);
-            this.label8.TabIndex = 16;
-            this.label8.Text = "User";
-            // 
-            // dtpBirth
-            // 
-            this.dtpBirth.Location = new System.Drawing.Point(181, 227);
-            this.dtpBirth.Name = "dtpBirth";
-            this.dtpBirth.Size = new System.Drawing.Size(206, 20);
-            this.dtpBirth.TabIndex = 17;
-            this.dtpBirth.Value = new System.DateTime(1995, 9, 25, 4, 29, 0, 0);
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(74, 230);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(28, 13);
-            this.label9.TabIndex = 18;
-            this.label9.Text = "Birth";
-            // 
             // UserType
             // 
             this.UserType.Items.Add("Admin");
             this.UserType.Items.Add("SuperAdmin");
             this.UserType.Items.Add("Cliente");
-            this.UserType.Location = new System.Drawing.Point(181, 267);
+            this.UserType.Location = new System.Drawing.Point(180, 300);
             this.UserType.Name = "UserType";
             this.UserType.Size = new System.Drawing.Size(206, 20);
-            this.UserType.TabIndex = 22;
+            this.UserType.TabIndex = 5;
             this.UserType.SelectedItemChanged += new System.EventHandler(this.UserType_SelectedItemChanged);
+            // 
+            // UserTypeLabel
+            // 
+            this.UserTypeLabel.AutoSize = true;
+            this.UserTypeLabel.Location = new System.Drawing.Point(63, 307);
+            this.UserTypeLabel.Name = "UserTypeLabel";
+            this.UserTypeLabel.Size = new System.Drawing.Size(56, 13);
+            this.UserTypeLabel.TabIndex = 23;
+            this.UserTypeLabel.Text = "User Type";
+            this.UserTypeLabel.Click += new System.EventHandler(this.label2_Click);
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(481, 450);
+            this.Controls.Add(this.UserTypeLabel);
             this.Controls.Add(this.UserType);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.dtpBirth);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.txtUser);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.txtDNI);
             this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtREmail);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtRePassword);
             this.Controls.Add(this.txtName);
@@ -263,22 +192,15 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.TextBox txtRePassword;
         private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.TextBox txtREmail;
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.TextBox txtDNI;
-        private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btnSubmit;
-        private System.Windows.Forms.TextBox txtUser;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.DateTimePicker dtpBirth;
-        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.DomainUpDown UserType;
+        private System.Windows.Forms.Label UserTypeLabel;
     }
 }
